3fe25aaa1003c35624d62163265132dbb0d9ee9a
Denis Kashitsyn <kamikadze1024@yandex.ru>
2021-01-03 12:50:42 +0300

    Simple example of reading MMF.
    Signed-off-by: Denis Kashitsyn <kamikadze1024@yandex.ru>

--- FastBigFileProcessor.pro
+++ FastBigFileProcessor.pro
@@ -3,5 +3,7 @@ CONFIG += console c++14
 CONFIG -= app_bundle
 CONFIG -= qt
 
+LIBS += -lboost_system -lboost_filesystem -lboost_iostreams
+
 SOURCES += \
         main.cpp
--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <boost/iostreams/device/mapped_file.hpp>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    boost::iostreams::mapped_file_source f;
+    f.open("shortTestData.txt");
+
+    const char *pStartPoint = static_cast<const char *>(f.data());
+    char arr[255];
+
+    if(f.is_open()) {
+        for(std::size_t i = 0; i < f.size(); i++) {
+            arr[i] = *(pStartPoint + i);
+            if(arr[i] == '\n') {
+                std::string res = arr;
+                std::cout << arr << std::endl;
+            }
+        }
+    } else {
+        std::cout << "Файл не открыт" << std::endl;
+    }
+
     return 0;
 }
