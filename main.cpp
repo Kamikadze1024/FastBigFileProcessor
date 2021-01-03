e0ffcafed2287c3f6ec3a33c890167fc32ffe25c
Denis Kashitsyn <kamikadze1024@yandex.ru>
2021-01-03 13:20:23 +0300

    Reading std strings from MMF.
    Signed-off-by: Denis Kashitsyn <kamikadze1024@yandex.ru>

--- main.cpp
+++ main.cpp
@@ -8,15 +8,21 @@ int main() {
     f.open("shortTestData.txt");
 
     const char *pStartPoint = static_cast<const char *>(f.data());
-    char arr[255];
+    char arr[256];
+    memset(arr, 0, 256);
 
     if(f.is_open()) {
+        std::size_t strCntr = 0;
         for(std::size_t i = 0; i < f.size(); i++) {
-            arr[i] = *(pStartPoint + i);
-            if(arr[i] == '\n') {
+            arr[strCntr] = *(pStartPoint + i);
+            if(*(pStartPoint + i) == '\n') {
                 std::string res = arr;
-                std::cout << arr << std::endl;
+                std::cout << res;
+                strCntr = 0;
+                memset(arr, 0, 256);
+                continue;
             }
+            strCntr++;
         }
     } else {
         std::cout << "Файл не открыт" << std::endl;
